8cba799afcf8b31f1895553e4e84bf2870179c2e
me-ch1cken <rdecroo24@gmail.com>
2025-04-20 16:11:33 +0200

    translated testimonial section

--- app/assets/testimonials.ts
+++ app/assets/testimonials.ts
@@ -1,6 +1,6 @@
 export const testimonials = [
     {
-        quote: 'Tijdens zijn drie maanden stage bij Panenco als softwareontwikkelaar heeft Robbe Decroo een sterke indruk nagelaten. Hij was bijzonder punctueel en betrouwbaar in zijn werk, en wist op korte tijd een merkbaar verschil te maken binnen het project waarop hij meewerkte. Zowel zijn technische vaardigheden als zijn communicatiestijl waren van hoog niveau, waardoor hij vlot samenwerkte met het team en snel zelfstandig verantwoordelijkheid kon opnemen. Een waardevolle toevoeging aan ons team!',
+        quote: 'During his three-month internship at Panenco as a software developer, Robbe Decroo made a strong impression. He was exceptionally punctual and reliable in his work, and he managed to make a noticeable impact on the project he contributed to in a short amount of time. Both his technical skills and communication style were of a high standard, allowing him to collaborate smoothly with the team and quickly take on responsibilities independently. A valuable addition to our team!',
         name: 'Egwin Avau',
         designation: 'CEO at Panenco',
         src: 'testimonials/egwin-panenco.png'
