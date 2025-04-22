ee511eb3ec72f2b2109e866f21b668b4732a0502
me-ch1cken <rdecroo24@gmail.com>
2025-04-22 23:34:55 +0200

    improved new layout for project items

--- components/slide-item.tsx
+++ components/slide-item.tsx
@@ -9,31 +9,20 @@ interface SlideItemProps {
     tech: string[];
 }
 
-export default function SlideItem({name, description, tech, imageUrl, projectUrl, githubUrl}: SlideItemProps) {
+export default function SlideItem({ name, description, tech, imageUrl, projectUrl, githubUrl }: SlideItemProps) {
     return (
-        <HoverImage src={imageUrl} alt={name} overlayText={name} />
+        <div className="relative w-64 h-40 group overflow-hidden rounded-xl">
+            <img
+                src={imageUrl}
+                alt={name}
+                className="w-full h-full object-contain bg-white transition duration-300 ease-in-out group-hover:brightness-20"
+            />
+            <div className="absolute inset-0 ps-4 pt-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-white text-lg font-semibold">
+                <h5>{name}</h5>
+                <p className="text-white font-extralight text-sm">{description}</p>
+                <a className="mt-2 me-2 text-blue-300 text-sm underline cursor-pointer hover:text-blue-400" href={projectUrl}>View project</a>-
+                <a className="ms-2 text-sm underline text-blue-300 cursor-pointer hover:text-blue-400" href={githubUrl}>View source code</a>
+            </div>
+        </div>
     );
 }
-
-import React from 'react';
-
-interface HoverImageProps {
-  src: string;
-  alt: string;
-  overlayText: string;
-}
-
-const HoverImage: React.FC<HoverImageProps> = ({ src, alt, overlayText }) => {
-  return (
-    <div className="relative w-64 h-40 group overflow-hidden rounded-xl">
-      <img
-        src={src}
-        alt={alt}
-        className="w-full h-full object-contain bg-white transition duration-300 ease-in-out group-hover:brightness-20"
-      />
-      <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-white text-lg font-semibold">
-        {overlayText}
-      </div>
-    </div>
-  );
-};
