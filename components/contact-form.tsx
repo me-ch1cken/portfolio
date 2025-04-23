0920f923b518bf280ad51fcd9e71eb8e82eb644a
me-ch1cken <rdecroo24@gmail.com>
2025-04-23 19:40:31 +0200

    general improvents / code cleanup

--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -33,7 +33,7 @@ export default function ContactForm() {
         setSubmitStatus('idle');
         
         try {
-            const response = await fetch(process.env.CONTACT_URL!, {
+            const response = await fetch(process.env.NEXT_PUBLIC_CONTACT_URL!, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
