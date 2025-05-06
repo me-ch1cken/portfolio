5d7b6790d6e05214e313f93438d083407b8bde32
me-ch1cken <rdecroo24@gmail.com>
2025-05-06 20:17:16 +0200

    added new project to list + styling issues fixed

--- app/assets/projects.ts
+++ app/assets/projects.ts
@@ -6,5 +6,13 @@ export const projects = [
         projectUrl: '',
         githubUrl: 'https://github.com/me-ch1cken/transaction-tracker',
         tech: ['Next.js', 'Typescript', 'Tailwind CSS', 'PostgreSQL', 'Docker']
+    },
+    {
+        name: 'SoundLoad',
+        description: 'An API linked with a webapp to download music from SoundCloud.',
+        imageUrl: 'projects/soundload.png',
+        projectUrl: '',
+        githubUrl: 'https://github.com/me-ch1cken/PYTHON-soundcloud-downloader',
+        tech: ['Python', 'FastAPI', 'React', 'Tailwind CSS']
     }
 ]
